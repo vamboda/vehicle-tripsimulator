d406b49cad3564206af74c42e5ca24eccdf9063b
vaboda <vaboda@VABODA-PC001>
2018-07-28 21:26:06 -0700

    Adding tripsimulator new version, which can simulate two vehicles moving

--- tripsimulation_proxy_service.js
+++ tripsimulation_proxy_service.js
@@ -3,9 +3,9 @@ const port = 5000;
 var express = require("express");
 var SignalRJS = require("signalrjs");
 const bodyParser = require("body-parser");
-var cors = require('cors');
+var cors = require("cors");
 
-const newLocal = './data/route.json';
+const newLocal = "./data/route.json";
 
 var route = require(newLocal);
 
@@ -14,11 +14,13 @@ var signalR = SignalRJS();
 var server = express();
 var router = express.Router();
 
-server.use(cors({
-  origin: 'http://localhost:3000',
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true
-}));
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+  })
+);
 
 server.use(signalR.createListener());
 server.use(bodyParser.json());
@@ -29,21 +31,23 @@ server.use(
 );
 
 /* serves main page */
-server.get("/", function (req, res) {
-  res.sendfile('index.htm');
+server.get("/", function(req, res) {
+  res.sendfile("index.htm");
 });
 
-let currentLocationIndex = 0;
+let shuttle1LocationIndex = 0;
+
+let shuttle2LocationIndex = 51;
 
 /* serves main page */
-server.get("/vehiclelocation", function (req, res) {
-  console.log("Current vehicle loc index:" + currentLocationIndex);
-  res.send(route[currentLocationIndex++ % route.length])
+server.get("/vehiclelocation", function(req, res) {
+  console.log("Current vehicle loc index:" + shuttle1LocationIndex);
+  res.send(route[shuttle1LocationIndex++ % route.length]);
 });
 
-server.get("/vehicles", function (req, res) {
+server.get("/vehicles", function(req, res) {
   /* some server side logic */
-  res.sendfile('data/vehicles.json');
+  res.sendfile("data/vehicles.json");
 });
 
 server.use("/", router);
@@ -51,15 +55,31 @@ console.log("Go to the link: http://localhost:" + port);
 
 server.listen(port);
 
-setInterval(function () {
-  var vehicleId = "Shuttle_1";
-  currentLocationIndex += 15;
-  var gpsLocation = route[currentLocationIndex % route.length];
-  console.log(`Moving the vehicle to the ${JSON.stringify(gpsLocation)}`);
-
+setInterval(function() {
+  var shuttle1Id = "Shuttle_1";
+  shuttle1LocationIndex += 15;
+  var shuttle1GpsLocation = route[shuttle1LocationIndex % route.length];
+  console.log(
+    `Moving the ${shuttle1Id} to the ${JSON.stringify(shuttle1GpsLocation)}`
+  );
   signalR.broadcast({
     Hub: "signalRNotificationHub",
     Method: "locationModified",
-    Args: [vehicleId, gpsLocation]
+    Args: [shuttle1Id, shuttle1GpsLocation]
   });
-}, 2000);
+}, 3359);
+
+setInterval(function() {
+  var shuttle2Id = "Shuttle_2";
+  shuttle2LocationIndex += 18;
+  var shuttle2GpsLocation = route[shuttle2LocationIndex % route.length];
+  
+  console.log(
+    `Moving the ${shuttle2Id} to the ${JSON.stringify(shuttle2GpsLocation)}`
+  );
+  signalR.broadcast({
+    Hub: "signalRNotificationHub",
+    Method: "locationModified",
+    Args: [shuttle2Id, shuttle2GpsLocation]
+  });
+}, 42);
